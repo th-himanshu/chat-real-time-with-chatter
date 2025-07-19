9d592fc7fc8ef641bf167e076cbff04415bafe7b
Himanshu <agrawalh2021@gmail.com>
2025-07-19 23:10:30 +0530

    some changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,6 +28,12 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+app.get("/", (req, res) => {
+  return res.json({
+    message: "This is from the root"
+  })
+})
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
